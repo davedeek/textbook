d64ad3cec799184b7f6c6ee5f917c71e11c7f4db
Dave Deek <ded480@g.harvard.edu>
2025-09-04 18:04:34 -0500

    Add @vercel/analytics dependency and update layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,17 @@
 import './global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
-import { Inter } from 'next/font/google';
+import { Literata } from 'next/font/google';
+import { Analytics } from "@vercel/analytics/next"
 import type { ReactNode } from 'react';
 
-const inter = Inter({
+const inter = Literata({
   subsets: ['latin'],
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
+      <Analytics />
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
